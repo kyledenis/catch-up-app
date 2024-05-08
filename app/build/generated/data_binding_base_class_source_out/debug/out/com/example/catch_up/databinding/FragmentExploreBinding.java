a747ef049f263de7cd2c7c6a6ac916005580f6a2
maxfricker <max.d.fricker@gmail.com>
2024-05-08 10:27:12 +1000

    still committing binaries, not sure how to correct .gitIgnore but at least I've got a map up in exploreFragment

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/example/catch_up/databinding/FragmentExploreBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/example/catch_up/databinding/FragmentExploreBinding.java
@@ -7,17 +7,25 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentContainerView;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import com.example.catch_up.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class FragmentExploreBinding implements ViewBinding {
   @NonNull
   private final FrameLayout rootView;
 
-  private FragmentExploreBinding(@NonNull FrameLayout rootView) {
+  @NonNull
+  public final FragmentContainerView googleMap;
+
+  private FragmentExploreBinding(@NonNull FrameLayout rootView,
+      @NonNull FragmentContainerView googleMap) {
     this.rootView = rootView;
+    this.googleMap = googleMap;
   }
 
   @Override
@@ -43,10 +51,19 @@ public final class FragmentExploreBinding implements ViewBinding {
 
   @NonNull
   public static FragmentExploreBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.google_map;
+      FragmentContainerView googleMap = ViewBindings.findChildViewById(rootView, id);
+      if (googleMap == null) {
+        break missingId;
+      }
 
-    return new FragmentExploreBinding((FrameLayout) rootView);
+      return new FragmentExploreBinding((FrameLayout) rootView, googleMap);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
--- app/src/main/java/com/example/catch_up/ExploreFragment.java
+++ app/src/main/java/com/example/catch_up/ExploreFragment.java
@@ -8,12 +8,58 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.CameraUpdateFactory;
+
 public class ExploreFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore, container, false);
+        // Initialise view
+        View view = inflater.inflate(R.layout.fragment_explore, container, false);
+        //Initialise map fragment
+        SupportMapFragment supportMapFragment = (SupportMapFragment)
+              getChildFragmentManager().findFragmentById(R.id.google_map);
+        //Async map
+        mapBehaviour(supportMapFragment);
+        //return view
+        return view;
+    }
+
+    private void mapBehaviour(SupportMapFragment supportMapFragment)
+    {
+		 //container method for map behaviour
+		 supportMapFragment.getMapAsync(new OnMapReadyCallback()
+		 {
+			 @Override
+			 public void onMapReady(GoogleMap googleMap)
+			 {
+				 googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener()
+				 {
+					 @Override
+					 public void onMapClick(LatLng latLng)
+					 {
+						 //When clicked on map
+						 //Initialise marker options
+						 MarkerOptions markerOptions = new MarkerOptions();
+						 //Set position of marker
+						 markerOptions.position(latLng);
+						 //Set title of marker
+						 markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+						 //remove old marker
+						 googleMap.clear();
+						 //animate zoom
+						 googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+						 //Add new marker to map
+						 googleMap.addMarker(markerOptions);
+					 }
+				 });
+			 }
+		 });
     }
 }
