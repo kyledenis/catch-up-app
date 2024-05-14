166f31033d041e008712aa338877f8db7e0693b1
Kyle Denis <91653791+kyledenis@users.noreply.github.com>
2024-05-14 20:00:32 +1000

    fix: remove binary files, update gradle version, remove API key

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.0") // Update this line with the latest version
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("com.android.tools.build:gradle:7.4.2") // Update this line with the latest version
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
         classpath("com.google.gms:google-services:4.3.10")
     }
 }
