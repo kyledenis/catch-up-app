6f37c0ddb257115e8026966b08bad2b369883fc7
maxfricker <max.d.fricker@gmail.com>
2024-05-14 20:39:29 +1000

    changed buildscript dependencies to library version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:8.0")
-        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+        classpath(libs.gradle)
+        classpath(libs.secrets.gradle.plugin)
     }
 }
 plugins {
